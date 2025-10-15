f765fdad45c6ceba8d6dd229f461908751b29728
“1NabilAkbar1” <nabilinformatika1@gmail.com>
2025-10-15 12:21:14 +0700

    WHILE

---
+++ Test.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double nilai;
+        double tertinggi = 0;
+        double terendah = 100;
+        int lulus = 0;
+        int tidakLulus = 0;
+        
+        for (int i = 1 ; i <= 10 ;i++) {
+            System.out.print("Masukkan nilai mahasiswa ke- " + i +": ");
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi){
+                tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
+        if (nilai >= 60) {
+            lulus++;
+        } else {
+            tidakLulus++;
+        }
+        }
+    
+        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " +lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " +tidakLulus);
+    }
+}
+    
+
